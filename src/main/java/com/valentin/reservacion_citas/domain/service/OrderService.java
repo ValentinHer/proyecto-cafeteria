fec8552441d4bf83f9f07a94ca9276ce9dccbb73
ValentinHer <valentinhernandezhernandez18@gmail.com>
2025-08-08 18:43:50 -0600

    [FIX] applicaciton.properties modificado en base a las credenciales de AWS y Paypal

--- src/main/java/com/valentin/reservacion_citas/domain/service/OrderService.java
+++ src/main/java/com/valentin/reservacion_citas/domain/service/OrderService.java
@@ -1,7 +1,6 @@
 package com.valentin.reservacion_citas.domain.service;
 
 import com.paypal.sdk.models.Order;
-import com.valentin.reservacion_citas.web.dto.request.OrderReqDto;
 
 public interface OrderService {
 	Order createOrder(String email);
