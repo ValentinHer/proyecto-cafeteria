56d9613ffcf7e02ac54979bfbb41e2131f68ba11
Valentin Hernandez <valentinhernandezhernandez18@gmail.com>
2025-12-06 20:33:03 -0600

    [ADD] lebreria para enviar emails SendGrid agregado

--- src/main/java/com/valentin/reservacion_citas/web/util/EmailSender.java
+++ src/main/java/com/valentin/reservacion_citas/web/util/EmailSender.java
@@ -1,21 +1,39 @@
 package com.valentin.reservacion_citas.web.util;
 
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Attachments;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.Base64;
+
 @Component
 public class EmailSender {
+
+    private final String sendGridKey;
+
+    private final SendGrid sendGrid;
+
 	private final JavaMailSenderImpl mailSender;
 
-	public EmailSender(JavaMailSenderImpl mailSender) {
-		this.mailSender = mailSender;
+	public EmailSender(@Value("${calendly.api.url}") String sendGridKey, JavaMailSenderImpl mailSender) {
+        this.sendGridKey = sendGridKey;
+        this.sendGrid = new SendGrid(sendGridKey);
+        this.mailSender = mailSender;
 	}
 
-	public void sendEmail(String html, String sendEmailTo, String subject) throws MessagingException {
+	/*public void sendEmail(String html, String sendEmailTo, String subject) throws MessagingException {
 		MimeMessage message = mailSender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(message, true);
 		helper.setTo(sendEmailTo);
@@ -26,5 +44,40 @@ public class EmailSender {
 		helper.addInline("logoImagen", image);
 
 		mailSender.send(message);
-	}
+	}*/
+
+    public void sendEmail(String html, String sendEmailTo, String subject) throws IOException {
+
+        Email from = new Email("TUCORREO@tudominio.com"); // remitente autorizado en SendGrid
+        Email to = new Email(sendEmailTo);
+
+        // Cargar imagen inline
+        ClassPathResource image = new ClassPathResource("/static/image (2).png");
+        byte[] imageBytes = image.getInputStream().readAllBytes();
+        String base64Image = Base64.getEncoder().encodeToString(imageBytes);
+
+        // Agregar imagen inline al HTML
+        String htmlWithImage = html.replace("cid:logoImagen", "cid:logoImagen");
+
+        Content content = new Content("text/html", htmlWithImage);
+        Mail mail = new Mail(from, subject, to, content);
+
+        // Crear adjunto inline
+        Attachments attachments = new Attachments();
+        attachments.setContent(base64Image);
+        attachments.setType("image/png");
+        attachments.setFilename("logo.png");
+        attachments.setDisposition("inline");
+        attachments.setContentId("logoImagen");
+
+        mail.addAttachments(attachments);
+
+        // Enviar request
+        Request request = new Request();
+        request.setMethod(Method.POST);
+        request.setEndpoint("mail/send");
+        request.setBody(mail.build());
+
+        sendGrid.api(request);
+    }
 }
